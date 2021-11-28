6ff5523c717c6a3c52c666b25c1a830f8ad430f2
Tochukwu Jonah <31604841+tochukwujonah@users.noreply.github.com>
2021-11-28 20:49:22 +0100

    Add files via upload

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
 
+import night from '../assets/images/png/003-night.png'
+
+const Bookmark = ()=> {
+    return (
+        <div className="bookmark-item">
+            <span>Enugu, Nigeria</span>
+
+            <div className="right-content">
+                <span>25<sup>o</sup>c</span>
+                <img src={night} alt="Night image" className="icon" />
+                
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Bookmark
--- src/components/tab-navigation.jsx
+++ src/components/tab-navigation.jsx
@@ -1,1 +1,35 @@
+import React from 'react'
+import { HomeOutlined, HomeRounded , SearchOutlined, LocationOnOutlined, BookmarkOutlined, BookmarkBorder} from '@material-ui/icons';
 
+import { useNavigate } from 'react-router';
+import { TABS } from '../screens/CONST'
+
+const TabNavigation = (props)=> {
+    const navigate = useNavigate();
+
+    const routes = ['home', 'location', 'search', 'bookmarks'];
+
+    const navigateScreen = (ev) => {
+        console.log(ev.target.nodeName)
+
+        if(ev.target.nodeName === 'svg') {
+            navigate(`/${routes[ev.target.id]}`);
+            return;
+        }
+
+        navigate(`/${routes[ev.target.parentElement.id]}`);
+
+    }
+
+
+    return (
+        <div className="tab-navigation">
+            <HomeOutlined className={props.active === 0 ? "active" : null} onClick={navigateScreen} id={TABS.home} /> 
+            <LocationOnOutlined className={props.active === 1 ? "active" : null} onClick={navigateScreen} id={TABS.location} />
+            <SearchOutlined className={props.active === 2 ? "active" : null} onClick={navigateScreen} id={TABS.search} /> 
+            <BookmarkBorder className={props.active === 3 ? "active" : null} onClick={navigateScreen} id={TABS.bookmark} />
+        </div>
+    )
+}
+
+export default TabNavigation
