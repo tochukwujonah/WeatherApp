451f8f8c4a36a5ed12246b0217e409b07dfc52d6
Tochukwu Jonah <31604841+tochukwujonah@users.noreply.github.com>
2021-11-28 20:41:50 +0100

    Add files via upload

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,1 +1,24 @@
+import {applyMiddleware, createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk'
+
+import { addFavouriteReducer, loadWeatherDataReducer, searchReducer } from './reducers';
+
+const initialState = {};
+const middleWares = [thunk];
+
+const rootReducer = combineReducers({
+    favourites: addFavouriteReducer,
+    weatherData: loadWeatherDataReducer,
+    search: searchReducer
+})
+
+const store = () => {
+    return createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(...middleWares)
+    );
+}
+
+export default store;
 
