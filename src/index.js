3e18c0bf800f95fc65b6fbf0aeb50947be382f03
Tochukwu Jonah <31604841+tochukwujonah@users.noreply.github.com>
2021-11-28 20:32:54 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { SplashScreen, HomeScreen, SearchScreen, LocationScreen, BookmarkScreen, LocationDetailsScreen } from './screens'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Routes>
+          <Route path='/' element={<SplashScreen />} />
+          <Route path='/home' element={<HomeScreen />} />
+          <Route path='/location' element={<LocationScreen />} />
+          <Route path="/location-details" element={<LocationDetailsScreen />} />
+          <Route path='/search' element={<SearchScreen />} />
+          <Route path='/bookmarks' element={<BookmarkScreen />} />
+        </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
